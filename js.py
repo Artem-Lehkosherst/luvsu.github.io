f9db8e035fab2dfbb5a783c72186b2e4f91abb16
Lthkosherst Artem <a.p.lehkosherst@student.itc.khai.edu>
2025-02-07 22:30:24 +0200

    Добавлены файлы сайта

---
+++ js.py
@@ -0,0 +1,13 @@
+from telegram import InlineKeyboardButton, InlineKeyboardMarkup, WebAppInfo
+from telegram.ext import Updater, CommandHandler
+
+def start(update, context):
+    web_app = WebAppInfo(url="https://yourdomain.com/yourapp.html")
+    keyboard = [[InlineKeyboardButton("Открыть открытку", web_app=web_app)]]
+    reply_markup = InlineKeyboardMarkup(keyboard)
+    update.message.reply_text("Нажми на кнопку, чтобы открыть открытку:", reply_markup=reply_markup)
+
+updater = Updater("7880413074:AAFkEYhsOqbmI2qt9vaGAPvEjNZQdM_c6Ik", use_context=True)
+updater.dispatcher.add_handler(CommandHandler("start", start))
+updater.start_polling()
+updater.idle()
